b2d8def5c07761867b1a6027e514d76b5dd59b28
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-31 12:08:10 +0500

    update

--- src/pages/Landing/Hero.jsx
+++ src/pages/Landing/Hero.jsx
@@ -5,7 +5,14 @@ const Hero = () => {
     <div className="w-full flex justify-center items-center border-b-8 border-solid border-[#222] flex-col relative isolate min-h-max pt-[100px] pb-[100px] sm:min-h-[733px]">
       <div className="w-full h-full absolute left-0 top-0 pointer-events-none -z-10">
         <div className="w-full bg-[rgba(0,0,0,.4)] h-full absolute top-0 left-0 z-10 bg-overlayHero"></div>
-        <img src="/hero-bg.jpg" className="w-full h-full object-cover" alt="" />
+        <video
+          src="/hero-desk.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover"
+        />
       </div>
       <div className="flex justify-center items-center flex-col gap-6 max-w-[950px] px-5 w-full">
         <h2 className="text-[2rem] sm:text-[4rem] leading-[1.1] text-center text-white font-medium max-w-[800px]">
